351ea6cffe52736901684d05b53647d9a7a6dbd8
AnthonyArminio <tonyarminio.0@gmail.com>
2025-06-18 17:23:28 -0400

    drew checkerboard

--- src/main/java/chess/application/Chess.java
+++ src/main/java/chess/application/Chess.java
@@ -5,18 +5,22 @@ import javafx.scene.Scene;
 import javafx.scene.layout.VBox;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.layout.GridPane;
 
 import chess.display.ChessBoard;
 
 public class Chess extends Application {
+
+    private final double boardSize = 500.0;
     private final String lightSquareColor = "#999999";
     private final String darkSquareColor = "#333333";
     private ChessBoard board;
 
-    
+
     public void init() {
         System.out.println("Test Init");
-        this.board = new ChessBoard(lightSquareColor, darkSquareColor);
+        this.board = new ChessBoard(boardSize, lightSquareColor, darkSquareColor);
     }
     
     public void start(Stage stage) {
@@ -27,6 +31,9 @@ public class Chess extends Application {
         
         stage.setTitle("Chess Application");
         stage.setScene(scene);
+
+        root.getChildren().add(this.board.getCheckerboard());
+
         stage.sizeToScene();
         stage.show();
     }
--- src/main/java/chess/display/ChessBoard.java
+++ src/main/java/chess/display/ChessBoard.java
@@ -1,7 +1,10 @@
 package chess.display;
 
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.GridPane;
 import javafx.scene.shape.Rectangle;
+
+import javafx.scene.paint.Paint;
+import javafx.scene.paint.Color;
 
 /**
  * Represents a grid of squares contained within a Pane object. The light square color and dark
@@ -9,13 +12,35 @@ import javafx.scene.shape.Rectangle;
  */
 public class ChessBoard {
 
-    private Pane pane;
+    private GridPane checkerboard;
+    private double squareSize;
+    private Color[] squareColors;
 
-    public ChessBoard(String lightSquareColor, String darkSquareColor) {
-        
+    public ChessBoard(double boardSize, String lightSquareColor, String darkSquareColor) {
+        this.squareSize = boardSize / 8.0;
+        checkerboard = new GridPane();
+        checkerboard.setPrefSize(boardSize, boardSize);
+
+        this.squareColors = new Color[2];
+        initializeColors(lightSquareColor, darkSquareColor);
+
+        makeSquares();
     }
 
-    public Pane getPane() {
-        return this.pane;
+    private void initializeColors(String lightSquareColor, String darkSquareColor) {
+        this.squareColors[0] = (Color) Paint.valueOf(lightSquareColor);
+        this.squareColors[1] = (Color) Paint.valueOf(darkSquareColor);
+    }
+
+    private void makeSquares() {
+        for (int row = 0; row < 8; row++) {
+            for (int col = 0; col < 8; col++) {
+                checkerboard.add(new Rectangle(this.squareSize, this.squareSize, squareColors[(row + col) % 2]), col, row);
+            }
+        }
+    }
+
+    public GridPane getCheckerboard() {
+        return this.checkerboard;
     }
 }
