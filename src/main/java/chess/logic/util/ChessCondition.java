08679c6032e91e075963f285a5bb7fefdff02943
AnthonyArminio <tonyarminio.0@gmail.com>
2025-06-26 20:55:17 -0400

    added javadoc comment to ChessCondition interface

--- src/main/java/chess/logic/util/ChessCondition.java
+++ src/main/java/chess/logic/util/ChessCondition.java
@@ -5,5 +5,14 @@ import chess.logic.ChessPiece;
 import chess.logic.ChessMove;
 
 public interface ChessCondition {
+
+    /**
+     * Test a condition as a function of a ChessPosition along with a start and end location.
+     * Should return true if the condition is satisfied, and false otherwise.
+     * @param position the context of the move to test
+     * @param start the start point of the move
+     * @param end the end point of the move
+     * @return true if a condition is satisfied, and false otherwise.
+     */
     public boolean test(ChessPosition position, int start, int end);
 }
